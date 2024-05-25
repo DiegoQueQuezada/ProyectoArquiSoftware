178ded1b137d857aabc1f01ea814a1d96a1c036d
Diego <diegodavid0404@hotmail.com>
2024-05-24 23:43:59 -0500

    Probando hooks

--- src/app/Componentes/1.login/login.component.spec.ts
+++ src/app/Componentes/1.login/login.component.spec.ts
@@ -18,4 +18,17 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should render the registration form when usuarioExiste is true', () => {
+    component.usuarioExiste = true;
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.formRegistro')).toBeTruthy();
+  });
+
+  it('should render the login form when usuarioExiste is false', () => {
+    component.usuarioExiste = false;
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.formEntrar')).toBeTruthy();
+  });
 });
